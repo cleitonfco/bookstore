4be450662026080dfb4712770ce70e4a09614b95
Cleiton Francisco <cleitonfco@gmail.com>
2014-07-27 20:38:03 -0300

    Rendering JSON

--- web.go
+++ web.go
@@ -1,18 +1,32 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "os"
+  "fmt"
+  "net/http"
+  "encoding/json"
+  "os"
 )
 
 func main() {
-    http.HandleFunc("/", hello)
-    fmt.Println("listening...")
-    err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
-    if err != nil {
-      panic(err)
-    }
+  http.HandleFunc("/", profiles)
+  fmt.Println("listening...")
+  err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+  if err != nil {
+    panic(err)
+  }
+}
+
+func profiles(w http.ResponseWriter, r *http.Request) {
+  profile := Profile{"Alex", []string{"Cleiton Francisco", "Programador"}}
+
+  js, err := json.Marshal(profile)
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+
+  w.Header().Set("Content-Type", "application/json")
+  w.Write(js)
 }
 
 func hello(res http.ResponseWriter, req *http.Request) {
